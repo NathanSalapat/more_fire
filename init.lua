28e004e4da8ebe75ca599006c6affb8a0e9b13f7
NathanSalapat <rendertracer@gmail.com>
2014-11-08 17:25:50 -0600

    formspecs are back

--- init.lua
+++ init.lua
@@ -1,29 +1,58 @@
 -- A couple variables used throughout.
 percent = 100
 -- GUI related stuff
-default.gui_bg = "bgcolor[#080808BB;true]"
-default.gui_bg_img = "background[5,5;1,1;gui_formbg.png;true]"
-default.gui_slots = "listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]"
-campfire = {}
+default.gui_bg = 'bgcolor[#080808BB;true]'
+default.gui_bg_img = 'background[5,5;1,1;gui_formbg.png;true]'
+default.gui_slots = 'listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]'
+
+more_fire = {}
+
+function default.get_hotbar_bg(x,y)
+	local out = ''
+	for i=0,7,1 do
+		out = out ..'image['..x+i..','..y..';1,1;gui_hb_bg.png]'
+	end
+	return out
+end
+
+function more_fire.campfire(pos, percent, item_percent)
+	local formspec =
+	'size[8,6.75]'..
+	default.gui_bg..
+	default.gui_bg_img..
+	default.gui_slots..
+	'background[5,5;1,1;campfire_active.png;true]'..
+	'list[current_name;fuel;1,1.5;1,1;]'..
+	'list[current_player;main;0,2.75;8,1;]'..
+	'list[current_player;main;0,4;8,3;8]'..
+	default.get_hotbar_bg(0,2.75)
+	return formspec
+end
+
+function more_fire.get_campfire_formspec(pos, percent)
+	local meta = minetest.get_meta(pos)local inv = meta:get_inventory()
+	local fuellist = inv:get_list('fuel')
+	if fuellist then
+		end
+	return more_fire.campfire(pos, percent, item_percent)
+end
 
 function burn(pointed_thing) --kindling doesn't always start from the first spark
-	ignite_chance = math.random(10)
+	ignite_chance = math.random(7)
 	if ignite_chance == 1
 		and string.find(minetest.get_node(pointed_thing.under).name, 'more_fire:kindling_contained')
 		then
-			minetest.env:swap_node(pointed_thing.under, {name = 'more_fire:contained_fire'})
+			minetest.swap_node(pointed_thing.under, {name = 'more_fire:embers_contained'})
 	elseif ignite_chance == 1
 		and string.find(minetest.get_node(pointed_thing.under).name, 'more_fire:kindling')
 		then
-			minetest.env:swap_node(pointed_thing.under, {name = 'more_fire:campfire'})
-
-	else --do nothing
+			minetest.swap_node(pointed_thing.under, {name = 'more_fire:embers'})
+	else --Do nothing
 	end
 end
 
--- Copied from the 3d_torch mod, changed a few things, and removed a couple lines.
-local function placeTorch(itemstack, placer, pointed_thing)
-			if pointed_thing.type ~= "node" then
+local function placeTorch(itemstack, placer, pointed_thing) -- Copied from 3d_torch mod, changed a few things, and removed a couple lines.
+			if pointed_thing.type ~= 'node' then
 				return itemstack
 			end
 
@@ -45,10 +74,10 @@ local function placeTorch(itemstack, placer, pointed_thing)
 			}
 			if p0.y<p1.y then
 			--place torch on floor
-			minetest.add_node(p1, {name="more_fire:torch"})
+			minetest.add_node(p1, {name='more_fire:torch'})
 			else
 			--place torch on wall
-			minetest.add_node(p1, {name="more_fire:torch_wall",param2=correct_rotation[param2]})
+			minetest.add_node(p1, {name='more_fire:torch_wall',param2=correct_rotation[param2]})
 			--return minetest.item_place(itemstack, placer, pointed_thing, param2)
 			end
 			itemstack:take_item()
@@ -56,9 +85,23 @@ local function placeTorch(itemstack, placer, pointed_thing)
 						
 end
 
--- abm that converts the already placed torches to the 3d ones copied from the 3d_torches mod
-minetest.register_abm({
-	nodenames = {"default:torch"},
+
+-- formspecs
+more_fire.embers_formspec =
+'size[8,6.75]'..
+default.gui_bg..
+default.gui_bg_img..
+default.gui_slots..
+'listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]'..
+'background[5,5;1,1;campfire_inactive.png;true]'..
+'list[current_name;fuel;1,1.5;1,1;]'..
+'list[current_player;main;0,2.75;8,1;]'..
+'list[current_player;main;0,4;8,3;8]'..
+default.get_hotbar_bg(0,2.75)
+
+-- ABMs
+minetest.register_abm({  -- Converts already placed torches to 3d ones copied from the 3d_torches mod.
+	nodenames = {'default:torch'},
 	interval = 1.0,
 	chance = 1,
 	action = function(pos, node)
@@ -71,11 +114,125 @@ minetest.register_abm({
 		}
 		print(node.param2)
 		if node.param2 == 1 then
-		minetest.swap_node(pos, {name="more_fire:torch"})
+		minetest.swap_node(pos, {name='more_fire:torch'})
 		else
-		minetest.swap_node(pos, {name="more_fire:torch_wall",param2=convert_facedir[node.param2]})
+		minetest.swap_node(pos, {name='more_fire:torch_wall',param2=convert_facedir[node.param2]})
 		end
 	end,
+})
+
+minetest.register_abm({  -- Controls non-contained fire
+	nodenames = {'more_fire:embers','more_fire:campfire'},
+	interval = 1.0,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		local meta = minetest.env:get_meta(pos)
+		for i, name in ipairs({
+		'fuel_totaltime',
+		'fuel_time',
+		}) do
+		if meta:get_string(name) == '' then
+			meta:set_float(name, 0.0)
+			end
+		end
+		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
+		local was_active = false
+		if meta:get_float('fuel_time') < meta:get_float('fuel_totaltime') then
+			was_active = true
+			meta:set_float('fuel_time', meta:get_float('fuel_time') + 0.25)
+			end
+		if meta:get_float('fuel_time') < meta:get_float('fuel_totaltime') then
+			minetest.sound_play({name='fire_small'},{gain=0.07},
+			{loop=true})
+			local percent = math.floor(meta:get_float('fuel_time') /
+			meta:get_float('fuel_totaltime') * 100)
+			meta:set_string('infotext','Campfire active: '..percent..'%')
+			minetest.swap_node(pos, {name = 'more_fire:campfire'})
+			meta:set_string('formspec',
+			'size[8,6.75]'..
+			default.gui_bg..
+			default.gui_slots..
+			'background[5,5;1,1;campfire_active.png;true]'..
+			'list[current_name;fuel;1,1.5;1,1;]'..
+			'list[current_player;main;0,2.75;8,1;]'..
+			'list[current_player;main;0,4;8,3;8]')
+			return
+			end
+			local fuel = nil
+			local fuellist = inv:get_list('fuel')
+			if fuellist then
+				fuel = minetest.get_craft_result({method = 'fuel', width = 1, items = fuellist})
+			end
+			if fuel.time <= 0 then
+				meta:set_string('infotext','Put more wood on the fire!')
+				minetest.swap_node(pos, {name = 'more_fire:embers'})
+				meta:set_string('formspec', more_fire.embers_formspec)
+			return
+		end
+		meta:set_string('fuel_totaltime', fuel.time)
+		meta:set_string('fuel_time', 0)
+		local stack = inv:get_stack('fuel', 1)
+		stack:take_item()
+		inv:set_stack('fuel', 1, stack)
+end,
+})
+
+minetest.register_abm({  -- Controls the contained fires.
+	nodenames = {'more_fire:embers_contained', 'more_fire:campfire_contained'},
+	interval = 1.0,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		local meta = minetest.env:get_meta(pos)
+		for i, name in ipairs({
+		'fuel_totaltime',
+		'fuel_time',
+		}) do
+		if meta:get_string(name) == '' then
+			meta:set_float(name, 0.0)
+			end
+		end
+		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
+		local was_active = false
+		if meta:get_float('fuel_time') < meta:get_float('fuel_totaltime') then
+			was_active = true
+			meta:set_float('fuel_time', meta:get_float('fuel_time') + 0.25)
+			end
+		if meta:get_float('fuel_time') < meta:get_float('fuel_totaltime') then
+			minetest.sound_play({name='fire_small'},{gain=0.07},
+			{loop=true})
+			local percent = math.floor(meta:get_float('fuel_time') /
+			meta:get_float('fuel_totaltime') * 100)
+			meta:set_string('infotext','Campfire active: '..percent..'%')
+			minetest.swap_node(pos, {name = 'more_fire:campfire_contained'})
+			meta:set_string('formspec',
+			'size[8,6.75]'..
+			default.gui_bg..
+			default.gui_slots..
+			'background[5,5;1,1;campfire_active.png;true]'..
+			'list[current_name;fuel;1,1.5;1,1;]'..
+			'list[current_player;main;0,2.75;8,1;]'..
+			'list[current_player;main;0,4;8,3;8]')
+			return
+			end
+			local fuel = nil
+			local fuellist = inv:get_list('fuel')
+			if fuellist then
+				fuel = minetest.get_craft_result({method = 'fuel', width = 1, items = fuellist})
+			end
+			if fuel.time <= 0 then
+				meta:set_string('infotext','Put more wood on the fire!')
+				minetest.swap_node(pos, {name = 'more_fire:embers_contained'})
+				meta:set_string('formspec', more_fire.embers_formspec)
+			return
+		end
+		meta:set_string('fuel_totaltime', fuel.time)
+		meta:set_string('fuel_time', 0)
+		local stack = inv:get_stack('fuel', 1)
+		stack:take_item()
+		inv:set_stack('fuel', 1, stack)
+end,
 })
 
 -- node definitions
@@ -97,7 +254,7 @@ minetest.register_node(':default:gravel', {
 		}
 	},
 	sounds = default.node_sound_dirt_defaults({
-		footstep = {name="default_gravel_footstep", gain=0.45},
+		footstep = {name='default_gravel_footstep', gain=0.45},
 	}),
 })
 
@@ -118,7 +275,7 @@ minetest.register_node('more_fire:torch', {
 	drop = 'default:torch',
 	sounds = default.node_sound_defaults(),
 	selection_box = {
-		type = "fixed",
+		type = 'fixed',
 		fixed = {-1/11, -1/2, -1/11, 1/11, 1/3, 1/11},
 	},
 })
@@ -140,7 +297,7 @@ minetest.register_node('more_fire:torch_wall', {
 	drop = 'default:torch',
 	sounds = default.node_sound_defaults(),
 	selection_box = {
-		type = "fixed",
+		type = 'fixed',
 		fixed = {-0.5, -0.3, -0.1, -0.5+0.3, 0.3, 0.1},
 	},
 })
@@ -150,6 +307,56 @@ minetest.register_node('more_fire:charcoal_block', {
 	tiles = {'more_fire_charcoal_block.png'},
 	is_ground_content = true,
 	groups = {oddly_breakable_by_hand=2,cracky=3},
+})
+
+minetest.register_node('more_fire:kindling', {
+	description = 'Kindling',
+	drawtype = 'mesh',
+	mesh = 'more_fire_kindling.obj',
+	tiles = {'more_fire_campfire_logs.png'},
+	inventory_image = 'more_fire_kindling.png',
+	wield_image = 'more_fire_kindling.png',
+	walkable = false,
+	is_ground_content = true,
+	groups = {dig_immediate=3, flammable=1,},
+	paramtype = 'light',
+	selection_box = {
+		type = 'fixed',
+		fixed = { -0.48, -0.5, -0.48, 0.48, -0.5, 0.48 },
+		},
+})
+
+minetest.register_node('more_fire:embers', {
+	description = 'Campfire',
+	drawtype = 'mesh',
+	mesh = 'more_fire_kindling.obj',
+	tiles = {'more_fire_campfire_logs.png'},
+	inventory_image = 'more_fire_campfire.png',
+	wield_image = 'more_fire_campfire.png',
+	walkable = false,
+	is_ground_content = true,
+	groups = {dig_immediate=3, flammable=1,},
+	paramtype = 'light',
+	drop = 'more_fire:kindling',
+	selection_box = {
+		type = 'fixed',
+		fixed = { -0.48, -0.5, -0.48, 0.48, -0.5, 0.48 },
+		},
+	on_construct = function(pos)
+			local meta = minetest.env:get_meta(pos)
+			meta:set_string('formspec', more_fire.embers_formspec)
+			meta:set_string('infotext', 'Campfire');
+			local inv = meta:get_inventory()
+			inv:set_size('fuel', 4)
+		end,
+	can_dig = function(pos, player)
+			local meta = minetest.get_meta(pos);
+			local inv = meta:get_inventory()
+			if not inv:is_empty('fuel') then
+				return false
+			end
+			return true
+end,
 })
 
 minetest.register_node('more_fire:campfire', {
@@ -166,49 +373,21 @@ minetest.register_node('more_fire:campfire', {
 	drop = 'more_fire:charcoal',
 	light_source = 14,
 	is_ground_content = true,
-	groups = {cracky=2,hot=2,attached_node=1,dig_immediate=3,igniter=1},
+	groups = {cracky=2,hot=2,attached_node=1,dig_immediate=3,igniter=1,not_in_creative_inventory=1},
 	selection_box = {
-		type = "fixed",
+		type = 'fixed',
 		fixed = { -0.48, -0.5, -0.48, 0.48, -0.5, 0.48 },
 		},
-})
-
-minetest.register_node('more_fire:contained_fire', {
-	description = 'Contained Campfire',
-	drawtype = 'mesh',
-	mesh = 'more_fire_contained_campfire.obj',
-	tiles = {
-		{name='fire_basic_flame_animated.png', animation={type='vertical_frames', aspect_w=16, aspect_h=16, length=1}}, {name='more_fire_campfire_logs.png'}},
-	inventory_image = 'more_fire_campfire_contained.png',
-	wield_image = 'more_fire_campfire_contained.png',
-	paramtype = 'light',
-	walkable = false,
-	damage_per_second = 1,
-	drop = 'more_fire:charcoal',
-	light_source = 14,
-	is_ground_content = true,
-	groups = {cracky=2,hot=2,attached_node=1,dig_immediate=3},
-		selection_box = {
-		type = "fixed",
-		fixed = { -0.48, -0.5, -0.48, 0.48, -0.5, 0.48 },
-		},
-})
-
-minetest.register_node('more_fire:kindling', {
-	description = 'Kindling',
-	drawtype = 'mesh',
-	mesh = 'more_fire_kindling.obj',
-	tiles = {'more_fire_campfire_logs.png'},
-	inventory_image = 'more_fire_kindling.png',
-	wield_image = 'more_fire_kindling.png',
-	walkable = false,
-	is_ground_content = true,
-	groups = {dig_immediate=3,flammable=1},
-	paramtype = 'light',
-	selection_box = {
-		type = "fixed",
-		fixed = { -0.48, -0.5, -0.48, 0.48, -0.5, 0.48 },
-		},
+	can_dig = function(pos,player)
+			local meta = minetest.env:get_meta(pos);
+			local inv = meta:get_inventory()
+			if not inv:is_empty('fuel') then
+				return false
+				end
+			return true
+			end,
+			get_staticdata = function(self)
+end,
 })
 
 minetest.register_node('more_fire:kindling_contained', {
@@ -223,9 +402,73 @@ minetest.register_node('more_fire:kindling_contained', {
 	groups = {dig_immediate=3,flammable=1},
 	paramtype = 'light',
 	selection_box = {
-		type = "fixed",
+		type = 'fixed',
 		fixed = { -0.48, -0.5, -0.48, 0.48, -0.5, 0.48 },
 		},
+})
+
+minetest.register_node('more_fire:embers_contained', {
+	description = 'embers',
+	drawtype = 'mesh',
+	mesh = 'more_fire_kindling_contained.obj',
+	tiles = {'more_fire_campfire_logs.png'},
+	walkable = false,
+	is_ground_content = true,
+	groups = {dig_immediate=3, flammable=1, not_in_creative_inventory=1},
+	paramtype = 'light',
+	drop = 'more_fire:kindling_contained',
+	inventory_image = 'more_fire_campfire_contained.png',
+	wield_image = 'more_fire_campfire_contained.png',
+	selection_box = {
+		type = 'fixed',
+		fixed = { -0.48, -0.5, -0.48, 0.48, -0.5, 0.48 },
+		},
+	on_construct = function(pos)
+			local meta = minetest.env:get_meta(pos)
+			meta:set_string('formspec', more_fire.embers_formspec)
+			meta:set_string('infotext', 'Campfire');
+			local inv = meta:get_inventory()
+			inv:set_size('fuel', 4)
+		end,
+	can_dig = function(pos, player)
+			local meta = minetest.get_meta(pos);
+			local inv = meta:get_inventory()
+			if not inv:is_empty('fuel') then
+				return false
+			end
+			return true
+end,
+})
+
+minetest.register_node('more_fire:campfire_contained', {
+	description = 'Contained Campfire',
+	drawtype = 'mesh',
+	mesh = 'more_fire_contained_campfire.obj',
+	tiles = {
+		{name='fire_basic_flame_animated.png', animation={type='vertical_frames', aspect_w=16, aspect_h=16, length=1}}, {name='more_fire_campfire_logs.png'}},
+	inventory_image = 'more_fire_campfire_contained.png',
+	wield_image = 'more_fire_campfire_contained.png',
+	paramtype = 'light',
+	walkable = false,
+	damage_per_second = 1,
+	drop = 'more_fire:charcoal',
+	light_source = 14,
+	is_ground_content = true,
+	groups = {cracky=2,hot=2,attached_node=1,dig_immediate=3,not_in_creative_inventory=1},
+		selection_box = {
+		type = 'fixed',
+		fixed = { -0.48, -0.5, -0.48, 0.48, -0.5, 0.48 },
+		},
+	can_dig = function(pos,player)
+			local meta = minetest.env:get_meta(pos);
+			local inv = meta:get_inventory()
+			if not inv:is_empty('fuel') then
+				return false
+				end
+			return true
+			end,
+			get_staticdata = function(self)
+end,
 })
 
 -- craft items
@@ -263,7 +506,7 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = 'more_fire:campfire 1',
+	output = 'more_fire:embers 1',
 	recipe = {
 		{'more_fire:kindling'},
 		{'default:torch'},
@@ -271,7 +514,7 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = 'more_fire:campfire 1',
+	output = 'more_fire:embers 1',
 	recipe = {
 		{'', '', ''},
 		{'default:stick', 'default:torch', 'default:stick'},
@@ -280,7 +523,7 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = 'more_fire:contained_fire 1',
+	output = 'more_fire:embers_contained 1',
 	recipe = {
 		{'default:cobble', 'default:cobble', 'default:cobble'},
 		{'default:cobble', 'more_fire:campfire', 'default:cobble'},
